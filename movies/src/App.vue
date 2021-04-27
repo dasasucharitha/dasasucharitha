8529b246d275ee2c57ee2b46939aedd2d51a9c55
dasasucharitha <83222140+dasasucharitha@users.noreply.github.com>
2021-04-27 12:47:58 +0530

    Add project movies

---
+++ movies/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <HelloWorld msg="Movies"/>
+  <button @click="topRated">Top Rated Movies</button>
+  <button @click="upcoming">Upcoming Movies</button>
+  <TopRatedMovies v-if="mflag == 'top_rated'"/>
+  <UpcomingMovies v-if="mflag == 'upcoming'"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TopRatedMovies from './components/TopRatedMovies.vue'
+import UpcomingMovies from './components/UpcomingMovies.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      mflag: null,
+      tflag: false,
+      uflag: false
+    }
+  },
+  components: {
+    HelloWorld,
+    TopRatedMovies,
+    UpcomingMovies
+  },
+  methods: {
+    topRated() {
+      this.mflag = 'top_rated'
+    },
+    upcoming() {
+      this.mflag = 'upcoming'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+button {
+  padding: 10px;
+  margin: 10px;
+  background-color: aquamarine;
+  border: 1px solid aquamarine;
+}
+</style>
---
+++ movies/src/components/UpcomingMovies.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="w100">
+    <h2>Upcoming Movies</h2>
+    <div class="dropdown movies" v-for="(r, i) in resData" :key="`A-${i}`">
+      <img :src="'https://image.tmdb.org/t/p/w500' + r.backdrop_path" :alt= "r.title"/>
+      <h4 class="m0"> {{ r.title }} <small>({{ r.release_date }})</small></h4>
+      <div class="dropdown-content">
+        <img :src="'https://image.tmdb.org/t/p/w500' + r.poster_path" :alt= "r.title" width="300" height="200"/>
+        <div class="desc">
+          <h4 class="m0" v-if="r.original_language != 'en'">{{ r.original_title }}</h4>
+          <p class="m0"><small>{{ r.overview }}</small></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+    data() {
+        return {
+            resData: [],
+        };
+    },
+    created () {
+        axios.get("https://api.themoviedb.org/3/movie/upcoming?api_key=a6f73dcd1cd14b814cd919c333fb33b5&language=en-US&page=1").then((data) => {
+            console.log("up -- res", data.data.results);
+            this.resData = data.data.results;
+        });
+    }
+};
+</script>
+<style scoped>
+.movies {
+  width: 30%;
+  height: 300px;
+  float: left;
+  margin: 20px 20px;
+}
+.m0 {
+  margin: 0px;
+}
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 4
+  33033000px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+.desc {
+  padding: 15px;
+  text-align: center;
+}
+</style>
